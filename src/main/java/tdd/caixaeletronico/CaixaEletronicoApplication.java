ee6e8dc89d69f4865ce39280c7e05433b7d05b4b
Antonio Jr <acdcjunior@gmail.com>
2017-05-11 14:03:05 -0300

    feat(app): adicionar CORS geral

--- src/main/java/tdd/caixaeletronico/CaixaEletronicoApplication.java
+++ src/main/java/tdd/caixaeletronico/CaixaEletronicoApplication.java
@@ -2,6 +2,10 @@ package tdd.caixaeletronico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class CaixaEletronicoApplication {
@@ -9,5 +13,15 @@ public class CaixaEletronicoApplication {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(CaixaEletronicoApplication.class, args);
 	}
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 
 }
